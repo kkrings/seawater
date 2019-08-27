a81a883d75be901b79407baa3d4b6efe1bac553a
Kai Krings <kai.krings@posteo.de>
2019-08-27 14:27:15 +0200

    Extract LCOV version from lcov executable.

--- cmake/FindLcov.cmake
+++ cmake/FindLcov.cmake
@@ -2,15 +2,28 @@
 # Lcov package
 #
 
+# Detect lcov executable.
 find_program(
 	Lcov_EXECUTABLE
 	lcov
+	DOC "lcov - a graphical GCOV front-end"
 	)
+
+# Extract LCOV version.
+if(Lcov_EXECUTABLE)
+	execute_process(
+		COMMAND ${Lcov_EXECUTABLE} --version
+		OUTPUT_VARIABLE Lcov_VERSION_OUTPUT
+		OUTPUT_STRIP_TRAILING_WHITESPACE
+		)
+
+	string(REGEX MATCH "[0-9.]+$" Lcov_VERSION ${Lcov_VERSION_OUTPUT})
+endif()
 
 include(FindPackageHandleStandardArgs)
 
 find_package_handle_standard_args(
 	Lcov
-	DEFAULT_MSG
-	Lcov_EXECUTABLE
+	REQUIRED_VARS Lcov_EXECUTABLE
+	VERSION_VAR Lcov_VERSION
 	)
