084494630a623a2669b56e31c201cbb332d79482
Kai Krings <kai.krings@posteo.de>
2019-09-17 13:35:48 +0200

    Put the Python bindings back.

--- README.md
+++ README.md
@@ -18,7 +18,7 @@ git clone https://github.com/kkrings/seawater.git
 ```
 
 The project's compilation is based on [CMake](https://cmake.org/) and requires
-at least version 3.10:
+at least version 3.12:
 
 ```sh
 mkdir seawater/build
@@ -64,4 +64,44 @@ ctest
 lcov -c -d . -o test.info
 lcov -r test.info '*/tests/*' '/usr/include/*' -o coverage.info
 genhtml -o coverage coverage.info
+```
+
+### Python module: *pyseawater*
+
+The C++ library can also be exported to Python by turning on the `cmake`
+option `SEAWATER_BUILD_PYTHON_MODULE`. The Python bindings are based
+on [Boost](https://www.boost.org/)'s *Python* library and require at least
+version 1.67. The following example shows how to add the *pyseawater* module to
+a dedicated active virtual Python environment, which was created via
+the [virtualenvwrapper](https://virtualenvwrapper.readthedocs.io/en/stable/):
+
+```sh
+cmake \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DCMAKE_INSTALL_PREFIX=$VIRTUAL_ENV \
+  -DSEAWATER_BUILD_PYTHON_MODULE=ON \
+  ..
+make install
+```
+
+By default, the C++ library will not be found when importing the Python module.
+This can be fixed by appending the virtual Python environment to
+the `LD_LIBRARY_PATH`:
+
+```sh
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$VIRTUAL_ENV/lib"
+```
+
+After building and installing the *pyseawater* module, the chemical
+composition (meaning the element mass fractions) of seawater can be calculated
+via:
+
+```python
+import pyseawater
+
+seawater = pyseawater.seawater(salinity=35e-3)
+
+fractions = seawater.fractions()
+for element, fraction in fractions.items():
+    print(element.symbol, fraction)
 ```
---
+++ python/element.cxx
@@ -0,0 +1,98 @@
+/**
+ *
+ * Implementation of Python binding for Element class
+ *
+ * (c) 2019
+ * Kai Krings
+ *
+ * @file element.cxx
+ * @date 2019-04-25
+ * @author Kai Krings <kai.krings@posteo.de>
+ *
+ */
+#include "seawater/element.h"
+
+#include <boost/noncopyable.hpp>
+#include <boost/python.hpp>
+
+namespace bp = boost::python;
+
+namespace seawater {
+namespace python {
+
+// register Element class
+void RegisterElement()
+{
+    bp::class_<
+        Element,
+        ElementPtr,
+        boost::noncopyable
+    >
+    {
+        "Element",
+        "Chemical element\n"
+        "\n"
+        "This class represents a chemical element, which has a specific\n"
+        "atomic charge and weight. Each element is identified by a specific\n"
+        "symbol.",
+        bp::init<const std::string&, int, double>
+        {
+            bp::args("self", "symbol", "charge", "weight"),
+            "Construct a new element.\n"
+            "\n"
+            "Parameters\n"
+            "----------\n"
+            "symbol : string\n"
+            "    Element's symbol\n"
+            "charge : int\n"
+            "    Element's atomic charge\n"
+            "weight : float\n"
+            "    Element's atomic weight"
+        }
+    }
+    .add_property(
+        "symbol",
+        &Element::GetSymbol,
+        "str: Symbol representing the chemical element"
+    )
+    .add_property(
+        "charge",
+        &Element::GetCharge,
+        "int: Atomic charge Z"
+    )
+    .add_property(
+        "weight",
+        &Element::GetWeight,
+        "float: Atomic weight A"
+    )
+    .add_static_property(
+        "min_charge",
+        &Element::GetMinCharge,
+        "int: Minimum allowed atomic charge"
+    )
+    .add_static_property(
+        "min_weight",
+        &Element::GetMinWeight,
+        "float: Minimum allowed atomic weight"
+    )
+    .def(
+        bp::self < bp::self
+        // "__lt__",
+        // &Element::operator<,
+        // bp::args("self", "element"),
+        // "Two chemical elements compare by their charges."
+    )
+    .def(
+        bp::self > bp::self
+        // "__gt__",
+        // &Element::operator>,
+        // bp::args("self", "element"),
+        // "Two chemical elements compare by their charges."
+    );
+
+    bp::implicitly_convertible<ElementPtr, ElementConstPtr>();
+    bp::register_ptr_to_python<ElementConstPtr>();
+}
+
+} // namespace python
+} // namespace seawater
